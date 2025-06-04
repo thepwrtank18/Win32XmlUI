26d83848fec5d6ffb15300f72ea5af038f79d328
TheTank20 <57580668+thepwrtank18@users.noreply.github.com>
2025-06-04 10:49:02 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,17 +4,7 @@ setlocal
 REM Add TDM-GCC to PATH
 set PATH=C:\TDM-GCC-64\bin;%PATH%
 
-::REM Build static library
-::
-::g++ -c -std=c++17 -I. Win32XmlUI.cpp tinyxml2.cpp
-::ar rcs lib\Win32XmlUI_x64.a Win32XmlUI.o tinyxml2.o
-::
-::REM Build DLL
-::
-::g++ -std=c++17 -I. -shared -o lib\Win32XmlUI_x64.dll Win32XmlUI.cpp tinyxml2.cpp -Wl,--out-implib,Win32XmlUI_x64.dll.a -lgdi32 -lcomctl32 -DWIN32XMLUI_EXPORTS
-::
-::REM Move the dll.a file to the lib directory
-::move Win32XmlUI_x64.dll.a lib\Win32XmlUI_x64.dll.a
+mkdir lib
 
 ECHO Build 32-bit static library
 
@@ -22,7 +12,7 @@ g++ -c -std=c++17 -I. -m32 Win32XmlUI.cpp tinyxml2.cpp
 if exist "Win32XmlUI_x86.a" (
   move "Win32XmlUI_x86.a" "lib\Win32XmlUI_x86.a"
 )
-tree /f
+
 ar rcs lib\Win32XmlUI_x86.a Win32XmlUI.o tinyxml2.o
 
 del Win32XmlUI.o tinyxml2.o
@@ -33,14 +23,14 @@ g++ -std=c++17 -I. -shared -m32 -o lib\Win32XmlUI_x86.dll Win32XmlUI.cpp tinyxml
 if exist "Win32XmlUI_x86.dll.a" (
   move "Win32XmlUI_x86.dll.a" "lib\Win32XmlUI_x86.dll.a"
 )
-tree /f
+
 ECHO Build 64-bit static library
 
 g++ -c -std=c++17 -I. -m64 Win32XmlUI.cpp tinyxml2.cpp
 if exist "Win32XmlUI_x64.a" (
   move "Win32XmlUI_x64.a" "lib\Win32XmlUI_x64.a"
 )
-tree /f
+
 ar rcs lib\Win32XmlUI_x64.a Win32XmlUI.o tinyxml2.o
 
 del Win32XmlUI.o tinyxml2.o
@@ -51,5 +41,5 @@ g++ -std=c++17 -I. -shared -m64 -o lib\Win32XmlUI_x64.dll Win32XmlUI.cpp tinyxml
 if exist "Win32XmlUI_x64.dll.a" (
   move "Win32XmlUI_x64.dll.a" "lib\Win32XmlUI_x64.dll.a"
 )
-tree /f
+
 echo Library build complete. Use sample/build_sample.bat to build the example client. 
